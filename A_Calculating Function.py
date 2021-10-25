b52d4f9a816b0cb223bf3f04a98760900f8e9703
exponential-tortoise <rafat.joy99@gmail.com>
2021-10-26 01:09:03 +0600

    Accepted

--- A_Calculating Function.py
+++ A_Calculating Function.py
@@ -1,11 +1,7 @@
 n=int(input())
 
-sum=0 
-for i in range(1,n+1):
-    if i%2==1:
-        sum-=i 
-    else:
-        sum+=i
-
-print(sum)
+if n%2==0:
+    print(n//2)
+else:
+    print(-(n//2)-1)
 
