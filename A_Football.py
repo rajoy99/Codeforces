d9b987d0096783459dfda4294e52f347bb461938
exponential-tortoise <rafat.joy99@gmail.com>
2021-10-28 14:11:10 +0600

    Accepted

--- A_Football.py
+++ A_Football.py
@@ -7,10 +7,11 @@ for i in range(1,len(string)):
     if string[i-1]==string[i]:
         zcount+=1
         maxcount=max(zcount,maxcount) 
+        # print(i," ",zcount," ",maxcount)
     else:
         zcount=0
 
-if maxcount>=7:
+if maxcount>=6:
     print("YES")
 else:
     print("NO")
