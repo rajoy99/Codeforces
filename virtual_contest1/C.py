79c797a5662be67aa73c891722daee0ca2200966
rajoy99 <rafat.joy99@gmail.com>
2022-01-18 01:17:44 +0600

    wrong sol

--- virtual_contest1/C.py
+++ virtual_contest1/C.py
@@ -1,11 +1,13 @@
-def isPermutation(n,arr):
+import math
 
-    li=list(range(1,n+1))
-    arr.sort()
-    return li==arr 
+# def isPermutation(n,arr):
+
+#     li=list(range(1,n+1))
+#     arr.sort()
+#     return li==arr 
 
 
-print(isPermutation(4,[4,3,1,2]))
+# print(isPermutation(4,[4,3,1,2]))
 
 t=int(input())
 
@@ -15,6 +17,24 @@ for i in range(t):
     arr.sort()
     ideal=list(range(1,n+1))
 
-    for i in range()
+    i=0
+    while i<n:
+        while arr[i]>i+1:
+            arr[i]=math.floor(arr[i]/2) 
+            if arr[i]<i+1:
+                print("NO")
+                break 
+        i+=1
+
+    print("YES")
+    print(arr) 
+            
+
+
+
+    
+
+
+
 
 
