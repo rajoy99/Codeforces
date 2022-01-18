c7c99166a08090e06c110781a458cf90599f6921
rajoy99 <rafat.joy99@gmail.com>
2022-01-18 14:10:35 +0600

    Accepted

--- virtual_contest1/C.py
+++ virtual_contest1/C.py
@@ -3,22 +3,22 @@ t=int(input())
 for i in range(t):
     n=int(input())
     arr=list(map(int,input().split()))
-    arr.sort()
+    arr.sort(reverse=True)
     
-    isin=[False for i in range(n)]
+    isin=[False for i in range(n+1)]
 
+    flag=True
     for i in arr:
         x=i
-        while x>n or isin[x]==False:
+        while x>n or isin[x]:
             x=x//2 
-            
+        if x>0:
+            isin[x]=True
+        else:
+            flag=False 
 
-    i=0
 
-
-    print("YES")
-    print(arr) 
-            
+    print("YES") if flag else print("NO")
 
 
 
